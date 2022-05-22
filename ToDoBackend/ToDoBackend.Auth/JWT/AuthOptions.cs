274eca888b3bc3455503fe4c7e8e20a33baf632e
Dmytro Sabadash <dmitriysabadash1@gmail.com>
2022-05-22 21:14:26 +0300

    adding angular

---
+++ ToDoBackend/ToDoBackend.Auth/JWT/AuthOptions.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace ToDoBackend.Auth.JWT
+{
+    public class AuthOptions
+    {
+        public const string ISSUER = "AuthServer";
+        public const string AUDIENCE = "ResourceServer";
+        public const string KEY = "letkeybehere12334";
+        public const int LIFETIME = 3600;
+        public static SymmetricSecurityKey GetSymmetricSecurityKey()
+        {
+            return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(KEY));
+        }
+    }
+}
---
+++ ToDoBackend/ToDoBackend.Auth/Startup.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using ToDoBackend.Auth.Identity;
+using ToDoBackend.Auth.JWT;
+
+namespace ToDoBackend.Auth
+{
+    public class Startup
+    {
+        public IConfiguration Configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddDbContext<AuthContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("Authentication")));
+            services.AddDefaultIdentity<IdentityUser>(options =>
+                {
+                    options.Password.RequiredLength = 5;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequireDigit = false;
+                    options.Password.RequireNonAlphanumeric = false;
+                })
+                .AddEntityFrameworkStores<AuthContext>();
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.RequireHttpsMetadata = false;
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidIssuer = AuthOptions.ISSUER,
+                        ValidateAudience = true,
+                        ValidAudience = AuthOptions.AUDIENCE,
+                        ValidateLifetime = true,
+                        IssuerSigningKey = new SymmetricSecurityKey
+                            (Encoding.ASCII.GetBytes(AuthOptions.KEY)),
+                        ValidateIssuerSigningKey = true
+                    };
+                });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+ 
+            app.UseRouting();
+ 
+            app.UseAuthentication();
+            app.UseAuthorization();
+            
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
